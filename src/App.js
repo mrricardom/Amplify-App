2d03d1e2a0e65393b0ffed22e76ee9ce9bb218ef
Ricardo Martini <Partyandhot@Ricardos-MBP.fios-router.home>
2020-11-09 19:37:37 -0500

    changed body text

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import './App.css'
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <h1>Hello from V2</h1>
-      </header>
+      <h1>Hello from V2</h1>
     </div>
   )
 }
